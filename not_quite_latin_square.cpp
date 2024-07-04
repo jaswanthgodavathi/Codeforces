9a89f80058c3a3fb8855458c69babd00f7be4154
jaswanthgodavarthi <jaswanthgodavarthy@gmail.com>
2024-07-04 14:31:13 +0530

    Modified not_quite_latin_square file

--- not_quite_latin_square.cpp
+++ not_quite_latin_square.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    int arr1[3][3];
-    int j;
-    cin >> j >> endl;
-    
+    int t;
+    cin >> t;
+
+    for (int k = 0; k < t; k++)
+    {
+        int ca = 0, cb = 0, cc = 0;
+        char matrix[3][3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                cin >> matrix[i][j];
+
+                if (matrix[i][j] == 'A')
+                {
+                    ca += 1;
+                }
+                else if (matrix[i][j] == 'B')
+                {
+                    cb += 1;
+                }
+                else if (matrix[i][j] == 'C')
+                {
+                    cc += 1;
+                }
+            }
+        }
+
+        if (ca < 3)
+        {
+            cout << 'A' << endl;
+        }
+        else if (cb < 3)
+        {
+            cout << 'B' << endl;
+        }
+        else if (cc < 3)
+        {
+            cout << 'C' << endl;
+        }
+    }
+
+    return 0;
 }
